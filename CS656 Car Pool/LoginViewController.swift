2e7726e77453364e95918b80b544946b2756a17f
Paul Lorenz <paul@pauls-mbp.home>
2017-03-12 23:45:04 -0400

    added scroll views, keyboard events

--- CS656 Car Pool/LoginViewController.swift
+++ CS656 Car Pool/LoginViewController.swift
@@ -10,10 +10,41 @@ import UIKit
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var keyboardHeight: NSLayoutConstraint!
+    @IBOutlet weak var insideViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    static var widthReference = CGFloat()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillChangeFrame),
+                                               name: .UIKeyboardWillChangeFrame,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: .UIKeyboardWillHide,
+                                               object: nil)
+        
+        insideViewHeight.constant = -UIApplication.shared.statusBarFrame.height
+        
+        LoginViewController.widthReference = titleLabel.frame.width
+    }
+    
+    func keyboardWillChangeFrame(notification: NSNotification) {
+        if let value = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let rect = value.cgRectValue
+            self.keyboardHeight.constant = rect.height
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        self.keyboardHeight.constant = 0
+        self.view.layoutIfNeeded()
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,11 +52,21 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        if self.passwordTextField.text == "test" {
-            return true
+        if identifier == "Main" {
+            if self.passwordTextField.text == "test" {
+                return true
+            }
+            return false
         }
-        return false
+        return true
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        if true || self.prefersStatusBarHidden {
+            self.navigationController?.isNavigationBarHidden = true
+        }
+    }
 }
---
+++ CS656 Car Pool/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  CS656 Car Pool
+//
+//  Created by Paul Lorenz on 3/12/17.
+//  Copyright © 2017 Rasheed Azeez, Paul Lorenz, Benjamin Nichols, Somsai Veerareddy, Steven Dong. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    @IBOutlet weak var stackViewWidth: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        stackViewWidth.constant = LoginViewController.widthReference
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
+}
