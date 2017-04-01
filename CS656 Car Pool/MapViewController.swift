132f15410f8dccbe8297cf5b4226d3406aeb1afa
Paul Lorenz <paul@pauls-mbp.home>
2017-04-01 15:24:47 -0400

    added MapVC

---
+++ CS656 Car Pool/MapViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MapViewController.swift
+//  CS656 Car Pool
+//
+//  Created by Paul Lorenz on 3/28/17.
+//  Copyright © 2017 Rasheed Azeez, Paul Lorenz, Benjamin Nichols, Somsai Veerareddy, Steven Dong. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
