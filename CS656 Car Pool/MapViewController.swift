ca038ebdc3ae4befb56216519f3d0eb00b5fc84c
Rasheed Azeez <Rasheed@rasheeds-mbp.fios-router.home>
2017-04-08 22:27:59 -0400

    ABC

--- CS656 Car Pool/MapViewController.swift
+++ CS656 Car Pool/MapViewController.swift
@@ -8,27 +8,40 @@
 
 import UIKit
 import GooglePlaces
+import GoogleMaps
+import GooglePlacePicker
 
 class MapViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   var placesClient: GMSPlacesClient!
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBOutlet var NameLabel: UILabel!
+    @IBOutlet var AddressLabel: UILabel!
+    
+    // Add a UIButton in Interface Builder, and connect the action to this function.
+   
+    @IBAction func CurrentLocation(_ sender: UIButton) {
+        let center = CLLocationCoordinate2D(latitude: 37.788204, longitude: -122.411937)
+        let northEast = CLLocationCoordinate2D(latitude: center.latitude + 0.001, longitude: center.longitude + 0.001)
+        let southWest = CLLocationCoordinate2D(latitude: center.latitude - 0.001, longitude: center.longitude - 0.001)
+        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
+        let config = GMSPlacePickerConfig(viewport: viewport)
+        let placePicker = GMSPlacePicker(config: config)
+        
+        placePicker.pickPlace(callback: {(place, error) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let place = place {
+                self.NameLabel.text = place.name
+                self.AddressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
+                    .joined(separator: "\n")
+            } else {
+                self.NameLabel.text = "No place selected"
+                self.AddressLabel.text = ""
+            }
+        })
     }
-    */
-
 }
